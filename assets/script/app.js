70e855254766fdff03ec93e87c3bbf333961d687
Bernard <oyebenard@gmail.com>
2023-12-06 12:07:26 -0600

    Static Page complete

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,23 +1,70 @@
 'use strict';
 
-const dialog = document.querySelector('.dialog');
-const login = document.querySelector('.login');
-const submit = document.querySelector('.submit');
-const modalBg = document.querySelector('.modal-bg');
-const input = document.querySelector('.dialog form input')
+import { onEvent, select, selectAll, create, print } from "./utils.js";
 
-login.addEventListener('click', function() {
-    dialog.classList.remove('is-hidden');
-    dialog.classList.add('is-visible');
+const modalBg = select('.modal-bg');
+const modalOne = select('.modal-one');
+const acceptBtn = select('.accept');
+const settingsBtn = select('.settings');
+const modalTwo = select('.modal-two');
+const inputs = selectAll('.modal-two input');
+
+function showModal() {
+    modalOne.classList.add('block');
     modalBg.classList.add('modal-bg-dark');
-});
+}
 
-window.addEventListener('click', (event) => {
-    if (event.target == modalBg || event.target == submit) {
-        if (input.value !== "") {
-            dialog.classList.remove('is-visible');
-            dialog.classList.add('is-hidden');
-            modalBg.classList.remove('modal-bg-dark');
-        }
+onEvent('load', window, () => {
+    if (!document.cookie.length > 0) {
+        setTimeout(showModal, 2000);
     }
 });
+
+onEvent('click', settingsBtn, () => {
+    modalTwo.classList.add('block');
+});
+
+onEvent('click', acceptBtn, () => {
+    setCookie('Browser', getBrowser(), 15);
+    setCookie('Operating System', getOS(), 15);
+    print(document.cookie);
+    // print(getCookie('Browser'));
+});
+
+function getBrowser() {
+    let browserArr = navigator.userAgent.split(' ');
+    for (let i = browserArr.length - 1; i >= 0;  i--) {
+        if (browserArr[i].match(/firefox/i)) {
+            return `Firefox`;
+        } else if (browserArr[i].match(/edg/i)) {
+            return `Edge`;
+        } else {
+            return `Chrome`;
+        }
+    }
+}
+
+function getOS() {
+    if (navigator.userAgent.indexOf("Windows") != -1) {
+        return 'Windows';
+    } else if (navigator.userAgent.indexOf("Mac OS") != -1) {
+        return 'Mac OS';
+    } else {
+        return 'Linux';
+    } 
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// print(getCookie('Browser'));
+
+function setCookie(name, value, life) {
+    document.cookie = `${name}=${value}; path=/; max-age=${life}; SameSite=Lax`;
+}
+
