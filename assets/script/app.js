c7bddabdce915c0f5f9e1aa54f19c4fc79fa9e69
Bernard <oyebenard@gmail.com>
2023-12-06 12:34:46 -0600

    Set cookies functionality added

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,7 +7,7 @@ const modalOne = select('.modal-one');
 const acceptBtn = select('.accept');
 const settingsBtn = select('.settings');
 const modalTwo = select('.modal-two');
-const inputs = selectAll('.modal-two input');
+const saveBtn= select('.save');
 
 function showModal() {
     modalOne.classList.add('block');
@@ -26,10 +26,27 @@ onEvent('click', settingsBtn, () => {
 
 onEvent('click', acceptBtn, () => {
     setCookie('Browser', getBrowser(), 15);
-    setCookie('Operating System', getOS(), 15);
-    print(document.cookie);
+    setCookie('Operating system', getOS(), 15);
+    setCookie('Screen width', getScreenWidth(), 15);
+    setCookie('Screen height', getScreenHeight(), 15);
+    // print(document.cookie);
     // print(getCookie('Browser'));
 });
+
+onEvent('click', saveBtn, () => {
+    let inputs = selectAll('.modal-two input');
+    setPreferences(inputs);
+})
+
+function setPreferences(arr) {
+    let options = ['Browser', 'Operating System', 'Screen width', 'Screen Height']
+    let functions = [getBrowser(), getOS(), getScreenWidth(), getScreenHeight()];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].checked) {
+            setCookie(`${options[i]}`, functions[i], 15);
+        }
+    }
+}
 
 function getBrowser() {
     let browserArr = navigator.userAgent.split(' ');
@@ -54,6 +71,14 @@ function getOS() {
     } 
 }
 
+function getScreenHeight() {
+    return window.innerHeight;
+}
+
+function getScreenWidth() {
+    return window.innerWidth;
+}
+
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -68,3 +93,4 @@ function setCookie(name, value, life) {
     document.cookie = `${name}=${value}; path=/; max-age=${life}; SameSite=Lax`;
 }
 
+print(document.cookie);
